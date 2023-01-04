52b33c8cddaa24aae5a167f6b2d1c91dc406d85f
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-04 19:12:35 +0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
